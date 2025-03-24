256aa670625a2f3e8b49ac2285f3b2ad41faca30
erica.figueroa <erica.figueroa@globant.com>
2025-03-24 14:48:00 -0500

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ChallengeWeatherApp"
+include(":app")
+include(":data")
+include(":domain")
+include(":presentation")
+include(":common")
+include(":data:remote")
+include(":data:local")
